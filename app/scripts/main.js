95781f282e5387fba686a31663caa3c96cb965f9
axel rigaud <axel.rigaud@gmail.com>
2017-01-16 17:25:42 +0100

    resize

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,6 +4,21 @@ $(document).ready(function(){
   $('.loader-wrapper').fadeOut();
   $('.hero').fadeIn();
 
+  //video
+  var $iframe = $('iframe');
+  var videoRatio = $iframe.height()/$iframe.width();
+
+  function setVideoDimensions () {
+    $iframe.width($(window).width());
+    $iframe.height($(window).width()*videoRatio);
+  }
+
+  setVideoDimensions();
+
+  $(window).resize(function(){
+    console.log('resize');
+    setVideoDimensions();
+  });
   //ga
   var trackOutboundLink = function(url) {
     ga('send', 'event', 'outbound', 'click', url, {'hitCallback':
@@ -17,6 +32,7 @@ $(document).ready(function(){
     trackOutboundLink($(this).attr('href'));
   });
 
+  //mobile detection
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     $('.clouds').hide();
   }
